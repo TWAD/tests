6149e1469b1cf7689aa390eb825a009f2b4f96e9
TWAD <anthonydeniaud@gmail.com>
2013-09-13 15:51:37 +0200

    test notification resolve

--- Tests/Unitaires/CalculetteTest.php
+++ Tests/Unitaires/CalculetteTest.php
@@ -15,7 +15,7 @@ class CalculetteTest extends \PHPUnit_Framework_TestCase
     public function testAdd()
     {
         $calc = new Calculette();
-        $result = $calc->add(30, 20);
+        $result = $calc->add(22, 20);
 
         // vérifie que votre classe a correctement calculé!
         $this->assertEquals(42, $result);
